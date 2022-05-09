e22a0943cd38016d178c5d806dd522f2e6e0d185
RodrigoEmiliano27 <54250496+RodrigoEmiliano27@users.noreply.github.com>
2022-05-09 18:26:06 -0300

    sql files

--- my_sql/InitBanco.sql
+++
@@ -1,8 +0,0 @@
-Use N2_ecommerce;
-
-insert into tbTipoUsuario (id,Tipo) Values ('unjawhe767','Gerente');
-insert into tbTipoUsuario (id,Tipo) Values ('aiwbduaoia','Usuario');
-
-insert into tbStatusPedido (id,PedidoStatus) values ('aoiuhwda23','Pendente');
-insert into tbStatusPedido (id,PedidoStatus) values ('aiwujhac1235','Concluído');
-
