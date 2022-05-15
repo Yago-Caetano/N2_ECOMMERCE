02b62be34fddb0fde5c2dfdaf4bc990eeaf4d92c
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2022-05-15 17:34:53 -0300

    Imported Main front-end data

---
+++ views/js/TelaGerencia.js
@@ -0,0 +1,32 @@
+﻿
+
+//gerencia o click nos botões de navegação
+function navigateTo(itemSelected) {
+    console.log(itemSelected.id);
+    switch (itemSelected.id) {
+
+        case "item-user":
+            window.location.href = "/Usuario";
+            break;
+
+        case "item-prod":
+            window.location.href = "/Produtos";
+            break;
+
+        case "item-ped":
+            window.location.href = "/Pedidos";
+            break;
+
+        case "item-end":
+            window.location.href = "/Endereco";
+            break;
+
+        case "item-relat":
+            window.location.href = "/Relatorios";
+            break;
+
+        case "item-cat":
+            window.location.href = "/CategoriaProduto";
+            break;
+    }
+};
