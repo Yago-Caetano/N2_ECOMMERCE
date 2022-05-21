03860a30bfee6578797a2b22648cf20708d75e77
RodrigoEmiliano27 <54250496+RodrigoEmiliano27@users.noreply.github.com>
2022-05-21 16:37:52 -0300

    User viewes

--- views/js/api.js
+++ views/js/api.js
@@ -21,11 +21,42 @@
                         }).catch(err=>{
                             reject(err)
 
-                        })
+                        })                   
+            
+        }
+        
+    ) 
+    
+ }
+ async function executeGetWithAuthentication(params)
+ {
+    return new Promise(
+        function(resolve,reject){
+                let token=localStorage.getItem('accessToken');
+                if (token==null)
+                {
+                    reject('Usuário não autenticado!')
+                   
+                }
+                fetch(`http://localhost:8081/${params.route}`,   {
+                            headers: {
+                                'Authorization': 'Bearer '+token,
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                            }
+                            
+                        }).then(resp=>{
+                            if (!resp.ok) {
+                                throw new Error(`HTTP error: ${resp.status}`);
+                              }
+       
+                            const json = resp.json();
+                            resolve(json)
 
-  
-            
-            
+                        }).catch(err=>{
+                            reject(err)
+
+                        })                   
             
         }
         
